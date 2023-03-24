b56f824d2fa7cb19f95bc6d11e2394bbdb35d5b8
Eli Pedro <eli.pedro.silva@ccc.ufcg.edu.br>
2023-03-24 09:32:59 -0300

    @Repository fixed

--- src/main/java/com/ufcg/psoft/mercadofacil/repository/VolatilLoteRepository.java
+++ src/main/java/com/ufcg/psoft/mercadofacil/repository/VolatilLoteRepository.java
@@ -3,8 +3,11 @@ package com.ufcg.psoft.mercadofacil.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.ufcg.psoft.mercadofacil.model.Lote;
 
+@Repository
 public class VolatilLoteRepository implements LoteRepository<Lote, Long> {
 
    List<Lote> lotes = new ArrayList<>();
