317ec06b37adf70dfcad03b68f4795269b478cf0
Eli Pedro <eli.pedro.silva@ccc.ufcg.edu.br>
2023-03-29 11:30:23 -0300

    ProdutoRepository

---
+++ src/main/java/com/ufcg/psoft/mercadofacil/repository/ProdutoImplRepository.java
@@ -0,0 +1,13 @@
+package com.ufcg.psoft.mercadofacil.repository;
+
+public class ProdutoImplRepository<Produto, Long> implements ProdutoRepository<Produto, Long> {
+    @Override
+    public Produto find(Long l) {
+        return null;
+    }
+
+    @Override
+    public Produto update(Produto produto) {
+        return null;
+    }
+}
